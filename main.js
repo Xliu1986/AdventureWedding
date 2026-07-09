cb31130123d75cb5f27a46e2fba3cbfc57caf7dd
Xliu1986 <jkinthecart@outlook.com>
2026-07-09 10:00:05 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Adventure Wedding Started!");
+const button=document.getElementById("startButton");
+
+button.addEventListener("click",()=>{
+
+    alert("游戏开发中...\n欢迎来到《冒険の結婚式》");
+
+});
